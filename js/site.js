3f2af8537314c634529b796d0cbbfd2208893163
Faizan Saeed <74678633+cpfaizan@users.noreply.github.com>
2023-07-03 23:35:51 -0700

    Initial of highcourt page

---
+++ js/site.js
@@ -0,0 +1,27 @@
+﻿// Write your JavaScript code.
+
+$(function () {
+    $("#loaderbody").addClass('hide');
+
+    $(document).bind('ajaxStart', function () {
+        $("#loaderbody").removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $("#loaderbody").addClass('hide');
+    });
+});
+
+showInPopup = (url, title) => {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (res) {
+            $('#form-modal .modal-body').html(res);
+            $('#form-modal .modal-title').html(title);
+            $('#form-modal').modal('show');
+            // to make popup draggable
+            $('.modal-dialog').draggable({
+                handle: ".modal-header"
+            });
+        }
+    })
+}
